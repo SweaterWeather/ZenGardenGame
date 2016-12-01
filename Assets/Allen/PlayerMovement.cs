17dbd07cad798ed665c9aa87c93296397411ab15
Allen C. Gardei <gardeia@ferris.edu>
2016-12-01 00:31:01 -0500

    Basic Movement and Item selection prototype
    Implemented basic up down left right movenet and added game object we
    can use to detect if we can select an object.

---
+++ Assets/Allen/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speedScaler = .01f;
+    float walkDir;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	    if(Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            transform.position += new Vector3(0, Input.GetAxis("Vertical") * speedScaler); //UP + DOWN
+            transform.position += new Vector3(Input.GetAxis("Horizontal") * speedScaler, 0); //RIGHT + LEFT
+
+            walkDir = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * (180 / Mathf.PI);
+        }
+        
+
+
+
+        
+        print(Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * (180/Mathf.PI));
+        transform.rotation = Quaternion.Euler(0, 0, walkDir);
+    }
+}
