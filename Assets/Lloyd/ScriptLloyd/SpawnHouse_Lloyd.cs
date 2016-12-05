f941d752f7696424c5692fd992960bd009db2239
RukiYakutoshi16 <aznmayham@gmail.com>
2016-12-04 22:57:30 -0500

    Update
    -Finish Selection Code

--- Assets/Lloyd/ScriptLloyd/SpawnHouse_Lloyd.cs
+++ Assets/Lloyd/ScriptLloyd/SpawnHouse_Lloyd.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 /// <summary>
 /// this class spawns house and place them
 /// </summary>
@@ -13,6 +14,15 @@ public class SpawnHouse_Lloyd : MonoBehaviour {
     /// </summary>
     public GameObject house1;
     /// <summary>
+    /// the list of point where the house can spawn
+    /// </summary>
+    public List<GameObject> spawnPoint;
+
+    /// <summary>
+    /// what point is already used
+    /// </summary>
+    public List<int> usedLocation;
+    /// <summary>
     /// this house is the reference of the house that hasn't place yet
     /// </summary>
     GameObject houseRef;
@@ -20,7 +30,19 @@ public class SpawnHouse_Lloyd : MonoBehaviour {
     /// the step process of making house
     /// </summary>
     int makingHouseStep = 1;
-	
+
+    /// <summary>
+    /// the index of the location that the house can build on
+    /// </summary>
+
+    int indexOfSpawnLocation = 0;
+	/// <summary>
+    /// initiate used Location
+    /// </summary>
+    void Start()
+    {
+        usedLocation = new List<int>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -46,7 +68,7 @@ public class SpawnHouse_Lloyd : MonoBehaviour {
                 }
                 break;
             case 2:
-                moveHouse();
+                switchLocation();
 
                 break;
 
@@ -68,9 +90,34 @@ public class SpawnHouse_Lloyd : MonoBehaviour {
     /// <returns> return location of the object</returns>
     Vector3 getLocation()
     {
-        float x = 1;
-        float y = 1;
-        float z = -1;
+        indexOfSpawnLocation = 0;
+        if (usedLocation.Count > 0)
+        {
+
+            while (checkPoint(indexOfSpawnLocation))
+            {
+                print("eeee");
+                indexOfSpawnLocation++;
+                if (indexOfSpawnLocation > (spawnPoint.Count - 1))
+                {
+                    indexOfSpawnLocation = 0;
+                }
+
+            }
+        }
+
+
+        float camX = spawnPoint[indexOfSpawnLocation].transform.position.x;
+        float camY = spawnPoint[indexOfSpawnLocation].transform.position.y;
+
+
+        Camera.main.transform.position = new Vector3(camX, camY, Camera.main.transform.position.z);
+       // houseRef.transform.position = spawnPoint[indexOfSpawnLocation].transform.position;
+
+
+        float x = spawnPoint[indexOfSpawnLocation].transform.position.x;
+        float y = spawnPoint[indexOfSpawnLocation].transform.position.y;
+        float z = spawnPoint[indexOfSpawnLocation].transform.position.z;
         Vector3 location = new Vector3(x, y, z);
         return location;
     }
@@ -89,12 +136,95 @@ public class SpawnHouse_Lloyd : MonoBehaviour {
             Vector3 move = new Vector3(x, y, houseRef.transform.position.z);
 
             houseRef.transform.position = move;
-        }
-       
-
-       
-        
+        }    
     }
+    /// <summary>
+    /// switching the location of the house ref gameObject
+    /// </summary>
+    void switchLocation()
+    {
+       // indexOfSpawnLocation = 0;
+
+        if (Input.GetButtonDown("Horizontal") )
+        {
+            float direction = Input.GetAxisRaw("Horizontal");
+            Mathf.Clamp(indexOfSpawnLocation += (int)Input.GetAxisRaw("Horizontal"), 0, 1);
+            if (indexOfSpawnLocation > (spawnPoint.Count -1))
+            {
+                indexOfSpawnLocation = 0;
+            }
+            if (indexOfSpawnLocation < 0)
+            {
+                indexOfSpawnLocation = (spawnPoint.Count - 1);
+            }
+             if (usedLocation.Count>0)
+             {
+                
+                 while (checkPoint(indexOfSpawnLocation))
+                 {
+                    if (direction > 0)
+                    {
+                        indexOfSpawnLocation++;
+                        if (indexOfSpawnLocation > (spawnPoint.Count - 1))
+                        {
+                            print("eeee");
+                            indexOfSpawnLocation = 0;
+                        }
+                    }
+                    else
+                    {
+                        indexOfSpawnLocation--;
+                        if (indexOfSpawnLocation < 0)
+                        {
+                            print("eeee");
+                            indexOfSpawnLocation = (spawnPoint.Count - 1);
+                        }
+                    }
+                   
+                    
+                 }
+             }
+           
+
+            float camX = spawnPoint[indexOfSpawnLocation].transform.position.x;
+            float camY = spawnPoint[indexOfSpawnLocation].transform.position.y;
 
 
+            Camera.main.transform.position = new Vector3(camX, camY, Camera.main.transform.position.z);
+            houseRef.transform.position = spawnPoint[indexOfSpawnLocation].transform.position;
+
+        }
+
+        if (Input.GetButtonDown("Vertical"))
+        {
+            usedLocation.Add(indexOfSpawnLocation);
+
+            print(usedLocation.Count+"  "+ indexOfSpawnLocation);
+            makingHouseStep = 1;
+        }
+
+
+
+
+    }
+    /// <summary>
+    /// checking if the spawn is overlapping with other spawning point
+    /// </summary>
+    /// <param name="spawnLocationIndex"></param>
+    /// <returns>the index of location in spawn point</returns>
+    bool checkPoint(int spawnLocationIndex)
+    {
+        
+        for(int i =(usedLocation.Count-1); i >=0; i--)
+        {
+            print("eeee");
+            if (usedLocation[i] == spawnLocationIndex)
+            {
+                return true;
+            }
+            print("used Point " + usedLocation[i] + " spawnPointIndex " + spawnLocationIndex);
+        }
+
+        return false;
+    }
 }
