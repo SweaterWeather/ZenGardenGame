229768e98a5b259b94d2c827208aec407d73827a
Allen C. Gardei <gardeia@ferris.edu>
2016-12-12 20:29:21 -0500

    Began Merge of Branches

--- Assets/Allen/ToolController.cs
+++ Assets/Allen/ToolController.cs
@@ -36,16 +36,16 @@ public class ToolController : MonoBehaviour {
         if (Input.GetButtonDown("Hoe")) //E
         {
             print("HOE WAS USED");
-                      
+            //play hoe animation              
             if(selectorRotatorScript._dir >= 45 && selectorRotatorScript._dir < 135)
             {
-                Animate.HoeBackward();
+                Animate.HoeForwards();
             } else if (selectorRotatorScript._dir >= 135 && selectorRotatorScript._dir < 225)
             {
                 Animate.HoeLeft();
             } else if (selectorRotatorScript._dir >= 225 && selectorRotatorScript._dir < 315)
             {
-                Animate.HoeForwards();
+                Animate.HoeBackward();
             } else if (selectorRotatorScript._dir >= 315 || selectorRotatorScript._dir >= 0)
             {
                 Animate.HoeRight();
